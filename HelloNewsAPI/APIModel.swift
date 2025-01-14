efc9de125051e29703271883154aa3251a651215
Willy <willy548798@gmail.com>
2025-01-14 14:45:32 +0800

    NewsView v1

---
+++ HelloNewsAPI/APIModel.swift
@@ -0,0 +1,8 @@
+//
+//  APIModel.swift
+//  HelloNewsAPI
+//
+//  Created by Willy Hsu on 2025/1/13.
+//
+
+import Foundation
---
+++ HelloNewsAPI/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  HelloNewsAPI
+//
+//  Created by Willy Hsu on 2025/1/13.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
