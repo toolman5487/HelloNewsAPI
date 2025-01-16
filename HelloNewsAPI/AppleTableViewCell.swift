368bce8f5edd16f1205c808cbb40744c8bcc6f4b
Willy <willy548798@gmail.com>
2025-01-16 16:13:43 +0800

    AppleNews api v1

---
+++ HelloNewsAPI/AppleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AppleTableViewCell.swift
+//  HelloNewsAPI
+//
+//  Created by Willy Hsu on 2025/1/14.
+//
+
+import UIKit
+
+class AppleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HelloNewsAPI/AppleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppleViewController.swift
+//  HelloNewsAPI
+//
+//  Created by Willy Hsu on 2025/1/14.
+//
+
+import UIKit
+
+class AppleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
